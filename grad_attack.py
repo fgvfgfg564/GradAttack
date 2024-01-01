aa823e2051a509d473b871e1387d4e170c28bdb8
Xinyu Hang <fgvfgfg564@126.com>
2024-01-01 04:37:57 +0000

    support 4D tensor

--- grad_attack.py
+++ grad_attack.py
@@ -19,26 +19,29 @@ def cosine_decay(init_lr, step, num_steps, alpha):
     a = (1. + torch.cos(torch.tensor(t * torch.pi))) * init_lr / 2
     return alpha * init_lr + (1-alpha) * a
 
-def attack(net: nn.Module, img: torch.Tensor, num_steps=100, init_lr=1e-3, epsilon=.01, lmbda=1.) -> torch.Tensor:
+def attack(net: nn.Module, img: torch.Tensor, criterion, num_steps=100, init_lr=1e-3, epsilon=.01) -> torch.Tensor:
     """
     Adversarial attack using Projected ADAM optimizer
     """
+    if len(img.shape) == 3:
+        no_batch = True
+        img = img.unsqueeze(0)
+    else:
+        no_batch = False
     img = img.detach().cuda()
-    # input = nn.Parameter(img.clone().detach().unsqueeze(0))
-    noise = nn.Parameter(epsilon*(torch.rand_like(img)-0.5).unsqueeze(0))
+    noise = nn.Parameter(epsilon*(torch.rand_like(img)-0.5))
     # print(input)
     net = net.cuda()
     optimizer = torch.optim.Adam([noise], init_lr)
     net.train()
-    criterion = RateDistortionLoss()
     for i in range(num_steps):
         optimizer.param_groups[0]['lr'] = cosine_decay(init_lr, i, num_steps, 0.01)
 
         noise_normed = normalize_l0(noise, epsilon)
-        input2 = torch.clamp(noise_normed + img.unsqueeze(0), 0., 1.)
+        input2 = torch.clamp(noise_normed + img, 0., 1.)
         out = net(input2)
         out["x_hat"] = torch.clamp(out["x_hat"], 0., 1.)
-        out_criterion = criterion(out, input2, lmbda)
+        out_criterion = criterion(out, input2)
         loss_rd = - out_criterion['loss']
         loss = loss_rd
 
@@ -52,6 +55,8 @@ def attack(net: nn.Module, img: torch.Tensor, num_steps=100, init_lr=1e-3, epsil
         loss.backward()
         optimizer.step()
     noise_normed = normalize_l0(noise, epsilon)
-    output = (noise_normed[0] + img).clamp(0., 1.).detach().cpu()
+    output = (noise_normed + img).clamp(0., 1.).detach().cpu()
     output = torch.round(output * 255) / 255.
+    if no_batch:
+        output = output[0]
     return output
